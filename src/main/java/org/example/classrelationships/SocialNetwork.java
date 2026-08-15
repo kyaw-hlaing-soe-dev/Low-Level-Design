1eb4f61993f7c5fb5dbe7e0b7b7674feb2573cbe
Kyaw Hlaing Soe <kyawhlaingsoe572@gmail.com>
2026-08-15 20:26:19 +0630

    test Social Network model

--- src/main/java/org/example/classrelationships/SocialNetwork.java
+++ src/main/java/org/example/classrelationships/SocialNetwork.java
@@ -51,4 +51,28 @@ class User {
 
 
 public class SocialNetwork {
+    public static void main(String[] args) {
+        User alice = new User("Alice");
+        User bob = new User("Bob");
+        User charlie = new User("Charlie");
+
+        alice.follow(bob);
+        alice.follow(charlie);
+        bob.follow(alice);
+
+        alice.sendMessage("Hello world!", "10:00 AM");
+        bob.sendMessage("Learning OOP!", "10:30 AM");
+
+        System.out.println(alice.getName() + " is following:");
+        for (User u : alice.getFollowing())
+            System.out.println("  - " + u.getName());
+
+        System.out.println(bob.getName() + "'s followers:");
+        for (User u : bob.getFollowers())
+            System.out.println("  - " + u.getName());
+
+        System.out.println(alice.getName() + "'s messages:");
+        for (Message m : alice.getMessages())
+            System.out.println("  [" + m.getTimestamp() + "] " + m.getContent());
+    }
 }
